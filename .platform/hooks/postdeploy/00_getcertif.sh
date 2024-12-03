a4ac3d05fe954a4acd47a5b268f313959a490c57
tchappy15 <153124713+tchappy15@users.noreply.github.com>
2024-12-02 17:31:11 -0700

    added HTTPS with certbot

--- .platform/hooks/postdeploy/00_getcertif.sh
+++ .platform/hooks/postdeploy/00_getcertif.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 # sudo certbot -n -d <your domain>  --nginx --agree-tos --email <your email>
+sudo certbot -n -d turtleshelterproject214.is404.net --nginx --agree-tos --email thomasch@byu.edu
